088b62e80e9ae4880480e821479cf143afa0ce43
[VM-QBL] <[1031647261@qq.com]>
2019-07-17 22:54:47 +0800

    Scrapy入门1-10

--- tutorial/spiders/quotes.py
+++ tutorial/spiders/quotes.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from tutorial.items import QuoteItem
 
 
 class QuotesSpider(scrapy.Spider):
@@ -8,4 +9,14 @@ class QuotesSpider(scrapy.Spider):
     start_urls = ['http://quotes.toscrape.com/']
 
     def parse(self, response):
-        pass
+        quotes = response.css('.quote')
+        print(quotes)
+        for quote in quotes:
+            item = QuoteItem()
+            item['text'] = quote.css('.text::text').get()
+            item['author'] = quote.css('.author::text').get()
+            item['tags'] = quote.css('.tags .tag::text').getall()
+            yield item
+        next = response.css('.pager .next a::attr("href")').get()
+        url = response.urljoin(next)
+        yield scrapy.Request(url=url, callback=self.parse)
