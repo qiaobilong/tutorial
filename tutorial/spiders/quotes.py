dc13db008bde62ce362bb5175f93bd779f99d3ca
Administrator <1031647261@qq.com>
2019-12-29 20:38:52 +0800

    构建项目管道，将数据存入mongodb中

--- tutorial/spiders/quotes.py
+++ tutorial/spiders/quotes.py
@@ -9,15 +9,16 @@ class QuotesSpider(scrapy.Spider):
     start_urls = ['http://quotes.toscrape.com/']
 
     def parse(self, response):
+        # 解析首页，并将数据传入Item
         quotes = response.css('.quote')
-        print(quotes)
         for quote in quotes:
             item = QuoteItem()
             item['text'] = quote.css('.text::text').get()
             item['author'] = quote.css('.author::text').get()
             item['tags'] = quote.css('.tags .tag::text').getall()
             yield item
+
+        # 解析下一页url，并传递给request，同时调用parse递归
         next = response.css('.pager .next a::attr("href")').get()
         url = response.urljoin(next)
-        # 递归调用parse，实现翻页解析
         yield scrapy.Request(url=url, callback=self.parse)
